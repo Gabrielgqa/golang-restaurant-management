5a32a9042656e622157513ef1118eac6717a9216
GabrielGQA <gabrielgqa@gmail.com>
2023-06-22 18:18:44 -0300

    feat: configure server and create routes

--- main.go
+++ main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/middlewares"
+	"golang-restaurant-management/routes"
+	"os"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middlewares.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	routes.Run(":" + port)
+}
--- routes/menuRouter.go
+++ routes/menuRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"restaurant-management/controllers"
+)
+
+func MenuRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/menus", controllers.GetMenus())
+	incomingRoutes.GET("/menus/:invoice_id", controllers.GetMenu())
+	incomingRoutes.POST("/menus", controllers.CreateMenu())
+	incomingRoutes.PATCH("/menus/:menu_id", controllers.UpdateMenu())
+}
